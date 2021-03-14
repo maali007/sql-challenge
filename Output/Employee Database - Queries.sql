9eb9cf05736ad631e6ebc8228bea1ae91bb7af23
maali007 <maali007@umn.edu>
2021-03-13 20:14:37 -0600

    Query for Question 2 added

--- Output/Employee Database - Queries.sql
+++ Output/Employee Database - Queries.sql
@@ -2,3 +2,8 @@
 SELECT "EMPLOYEES".emp_no, "EMPLOYEES".last_name, "EMPLOYEES".first_name, "EMPLOYEES".sex, "SALARIES".salary
 FROM "EMPLOYEES"
 JOIN "SALARIES" ON "EMPLOYEES".emp_no = "SALARIES".emp_no;
+
+-- Q2. List first name, last name, and hire date for employees who were hired in 1986.
+SELECT first_name, last_name, hire_date
+FROM "EMPLOYEES"
+WHERE hire_date LIKE '%1986';
