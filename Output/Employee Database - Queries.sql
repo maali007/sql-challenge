f94cf1307d5b28d79e433d7c02bc22ab53ba9c91
maali007 <maali007@umn.edu>
2021-03-13 20:41:28 -0600

    Query for Question 8 added

--- Output/Employee Database - Queries.sql
+++ Output/Employee Database - Queries.sql
@@ -41,3 +41,9 @@ JOIN "DEPT_EMP" ON "EMPLOYEES".emp_no = "DEPT_EMP".emp_no
 JOIN "DEPARTMENTS" ON "DEPT_EMP".dept_no = "DEPARTMENTS".dept_no
 WHERE "DEPARTMENTS".dept_name = 'Sales' or "DEPARTMENTS".dept_name = 'Development';
 
+-- Q8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT last_name, COUNT(last_name) AS "Frequency count"
+FROM "EMPLOYEES"
+GROUP BY last_name
+ORDER BY "Frequency count" DESC;
+
