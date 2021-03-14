83e238d3e21a25810adce7c54de0ab667b56bd17
maali007 <maali007@umn.edu>
2021-03-13 20:19:16 -0600

    Query for Question 3 added

--- Output/Employee Database - Queries.sql
+++ Output/Employee Database - Queries.sql
@@ -7,3 +7,10 @@ JOIN "SALARIES" ON "EMPLOYEES".emp_no = "SALARIES".emp_no;
 SELECT first_name, last_name, hire_date
 FROM "EMPLOYEES"
 WHERE hire_date LIKE '%1986';
+
+-- Q3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+SELECT "DEPT_MANAGER".dept_no, "DEPARTMENTS".dept_name, "DEPT_MANAGER".emp_no, "EMPLOYEES".last_name, "EMPLOYEES".first_name
+FROM "DEPT_MANAGER"
+JOIN "DEPARTMENTS" ON "DEPT_MANAGER".dept_no = "DEPARTMENTS".dept_no
+JOIN "EMPLOYEES" ON "DEPT_MANAGER".emp_no = "EMPLOYEES".emp_no;
+
